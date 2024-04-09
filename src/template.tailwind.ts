74e00abb5918051b3c57e568de2ade41a83dc15a
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-04-09 14:44:01 +0800

    fix: template.tailwind.ts

--- src/template.tailwind.ts
+++ src/template.tailwind.ts
@@ -1,6 +1,6 @@
 export default `/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts,tsx,.vue,.jsx,.svelte,.mdx}'],
+  content: ['./src/**/*.{html,ts,tsx,vue,jsx,svelte,mdx}'],
   theme: {
     extend: {},
   },
